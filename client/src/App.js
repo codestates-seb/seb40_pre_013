518242d88a96cf4f7fc971b2679e8fef033c7e17
GopOwl <viogarden16@gmail.com>
2022-11-01 20:44:47 +0900

    design: 질문조회 페이지 디자인 구현 98% 완료

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/header/header';
 import SignUp from './components/signup';
 import Main from "./pages/Main";
 import AskQuestion from "./components/AskQuestion";
+import QuestionLookup from "./components/questionLookup";
 
 
 function App() {
@@ -13,11 +14,12 @@ function App() {
       <div>
         <Header/>
         <Routes>
-          <Route path="/" element={<Main/>}></Route>
+          {/* <Route path="/" element={<Main/>}></Route> */}
           <Route path="/login" element={<Login />}></Route>
           <Route path="/signup" element={<SignUp/>}></Route>
           <Route path="/ask" element={<AskQuestion/>}></Route>
         </Routes>
+        <QuestionLookup/>
       </div>
     </BrowserRouter>
 
