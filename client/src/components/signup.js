3ffdc04979c1cb851c260f914eb9ec38a8f011d5
GopOwl <83641398+GopOwl@users.noreply.github.com>
2022-11-03 17:43:28 +0900

    Revert "[Client] 회원가입 기능 구현 "

--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -6,88 +6,9 @@ import { ReactComponent as QuestionLogo } from '../components/Icons/questionMark
 import { ReactComponent as PointLogo } from '../components/Icons/화살표.svg';
 import { ReactComponent as BookMarkLogo } from '../components/Icons/bookmark.svg';
 import { ReactComponent as TrophyLogo } from '../components/Icons/트로피.svg';
-import { Link, useNavigate } from 'react-router-dom';
-import { useState } from 'react';
-import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
-    // 초기값 - 이메일 , 닉네임, 비밀번호
-    const [inputId, setInputId] = useState('');
-    const [inputPw, setInputPw] = useState('');
-    const [inputName, setInputName] = useState('');
-
-    // 오류메세지 상태 
-    const [idMessage, setIdMessage] = useState("");
-    const [nameMessage, setNameMessage] = useState("");
-    const [passwordMessage, setPasswordMessage] = useState("");
-
-    // 유효성 검사 
-    const [isId, setIsId] =  useState(false);
-    const [isname, setIsName] =  useState(false);
-    const [isPassword, setIsPassword] =  useState(false);
-
-    // 정규 표현식을 이용한 유효성 검사 (ID)
-    const onChangeId = (e) => {
-        const currentId = e.target.value;
-        setInputId(currentId);
-        const idRegExp =
-          /^[A-Za-z0-9_]+[A-Za-z0-9]*[@]{1}[A-Za-z0-9]+[A-Za-z0-9]*[.]{1}[A-Za-z]{1,3}$/;
-     
-        if (!idRegExp.test(currentId)) {
-          setIdMessage("이메일의 형식이 올바르지 않습니다!");
-          setIsId(false);
-        } else {
-          setIdMessage("사용 가능한 이메일 입니다.");
-          setIsId(true);
-        }
-      };
-
-    // 정규 표현식을 이용한 유효성 검사 (닉네임)
-    const onChangeName = (e) => {
-        const currentName = e.target.value;
-        setInputName(currentName);
-
-        if (currentName.length < 2 || currentName.length > 5) {
-            setNameMessage("닉네임은 2글자 이상 5글자 이하로 입력해주세요!");
-            setIsName(false);
-        } else {
-            setNameMessage("사용가능한 닉네임 입니다.");
-            setIsName(true);
-        }
-    };
-
-    // 정규 표현식을 이용한 유효성 검사 (비밀번호)
-    const onChangePassword = (e) => {
-        const currentPassword = e.target.value;
-        setInputPw(currentPassword);
-        const passwordRegExp =
-            /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
-        if (!passwordRegExp.test(currentPassword)) {
-            setPasswordMessage(
-                "숫자+영문자+특수문자 조합으로 8자리 이상 입력해주세요!"
-            );
-            setIsPassword(false);
-        } else {
-            setPasswordMessage("안전한 비밀번호 입니다.");
-            setIsPassword(true);
-        }
-    };
-
-    const navigate = useNavigate();
-
-    const onClickLogin = (e) => {
-        e.preventDefault(); // 새로고침 방지 
-        axios.post('https://f78a-36-38-67-6.jp.ngrok.io/members', {
-            email: inputId,
-            displayName : inputName,
-            password: inputPw
-        })
-        .then(() => {
-            navigate('/login'); // 연결이 됬다면 로그인 페이지로 이동 
-        })
-        .catch()
-    }
-
     return (
         <SignUpStyle>
             <div className='d-flex flex__center snippet-hidden'>
@@ -135,23 +56,20 @@ const SignUp = () => {
                                 <div className='signupInput'>
                                     <label className="flex--item s-label" htmlFor="question-title">Display name</label>
                                     <div className="d-flex ps-relative">
-                                        <input className="flex--item s-input" type="text" id="question-title" value={inputName} onChange={onChangeName} />
+                                        <input className="flex--item s-input" type="text" id="question-title" />
                                     </div>
-                                    <div>{nameMessage}</div>
                                 </div>
                                 <div className='signupInput'>
                                     <label className="flex--item s-label" htmlFor="question-title">Email</label>
                                     <div className="d-flex ps-relative">
-                                        <input className="flex--item s-input" type="text" id="question-title" value={inputId} onChange={onChangeId} />
+                                        <input className="flex--item s-input" type="text" id="question-title" />
                                     </div>
-                                    <div>{idMessage}</div>
                                 </div>
                                 <div className='signupInput'>
                                     <label className="flex--item s-label" htmlFor="question-title">Password</label>
                                     <div className="d-flex ps-relative">
-                                        <input className="flex--item s-input" type="password" id="question-title" value={inputPw} onChange={onChangePassword} />
+                                        <input className="flex--item s-input" type="text" id="question-title" />
                                     </div>
-                                    <div>{passwordMessage}</div>
                                     <p className='pwCondition'>
                                         Passwords must contain at least eight characters, including at least 1 letter and 1 number.
                                     </p>
@@ -169,7 +87,7 @@ const SignUp = () => {
                                         <label className="flex--item s-label fw-normal" htmlFor="example-item">Opt-in to receive occasional product updates, user research invitations, company announcements, and digests.</label>
                                     </div>
                                 </fieldset>
-                                <button className="s-topbar--item s-topbar--item__unset ml4 s-btn s-btn__primary" onClick={onClickLogin}>Sign up</button>
+                                <a href="#" className="s-topbar--item s-topbar--item__unset ml4 s-btn s-btn__primary">Sign up</a>
                             </form>
                             <div className='js-terms fs-caption fc-light ta-left mt32'>
                                 By clicking “Sign up”, you agree to our <a href='#'>terms of service, privacy policy</a> and <a href='#'>cookie policy</a>
