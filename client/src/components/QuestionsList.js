45a7fa26475aa53e054262e64d8a1d61b800b25b
GopOwl <viogarden16@gmail.com>
2022-11-04 14:45:47 +0900

    feat: 로그인 해야 글작성과 제목 열람 가능하게 구현

--- client/src/components/QuestionsList.js
+++ client/src/components/QuestionsList.js
@@ -4,8 +4,15 @@ import styled from "styled-components";
 
 function QuestionsList({ author, title, body, createdAt }) {
   const navigate = useNavigate();
+  // 로그인을 했을 경우에만 Ask question 눌렀을때 질문 작성이 가능 그게 아니라면 로그인 화면으로 이동 
   const handleTitleClick = () => {
-    navigate("/qlookup");
+    if(sessionStorage.getItem('username') !== null) {
+      navigate("/qlookup");
+    }
+    else {
+      alert('로그인을 먼저 하고 오세요!');
+      window.location.href = '/login';
+    }
   };
 
   return (
