a4261b4c1af60fe6b97e265e583471f00fa69081
cohi <tkfka156@gmail.com>
2022-11-06 00:14:34 +0900

    refactor. 로그인 테스트 코드 수정

--- server/src/test/java/com/sebbe013/member/controller/LoginTest.java
+++ server/src/test/java/com/sebbe013/member/controller/LoginTest.java
@@ -1,5 +1,6 @@
 package com.sebbe013.member.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sebbe013.member.dto.LoginDto;
 import com.sebbe013.member.dto.MemberSignUpDto;
 import com.sebbe013.member.entity.Member;
@@ -10,33 +11,22 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.test.web.servlet.MockMvc;
 
-import java.net.URI;
-import java.net.URISyntaxException;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import static java.lang.String.format;
-import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) //내장 서버를 랜덤 포트로 띄운다. 테스트를 위한 서블릿 컨테이너를 띄운다.
+@SpringBootTest
 @ActiveProfiles("test")
+@AutoConfigureMockMvc
 public class LoginTest {
-    @LocalServerPort //로컬 서버 포트를 사용한다.
-    private int port;
 
-    private RestTemplate restTemplate = new RestTemplate(); //랜덤 포트를 사용하면 resttemplate을 사용해야한다. 서버간 통신??
-
-    private URI uri( String path ) throws URISyntaxException{ //uri를 생성한다.
-        return new URI(format("http://localhost:%d%s", port, path));
-    }
 
     @Autowired
     private MemberService memberService;
@@ -45,7 +35,7 @@ public class LoginTest {
     @Autowired
     private MemberRepository memberRepository;
     @Autowired
-    private PasswordEncoder passwordEncoder;
+    private MockMvc mockMvc;
 
     @BeforeEach
     void init(){
@@ -59,34 +49,41 @@ public class LoginTest {
     void 로그인_시도() throws Exception{
         //given
         LoginDto loginDto = LoginDto.builder().username("test@gmail.com").password("123@aadfend4").build();
-        HttpEntity<LoginDto> dto = new HttpEntity<>(loginDto);//logindto 내용이 바디값으로 들어간다??
-        ResponseEntity<String> response = restTemplate.exchange(uri("/members/login"), HttpMethod.POST, dto, String.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String dto = objectMapper.writeValueAsString(loginDto);
 
-        //then
-        assertThat(response.getStatusCodeValue()).isEqualTo(200);
+        mockMvc.perform(post("/members/login").contentType(MediaType.APPLICATION_JSON).content(dto))
+                .andExpect(status().isOk())
+                .andExpect(content().string("로그인 성공"))
+                .andDo(print());
 
-        System.out.println(response.getHeaders().get("Authorization"));
     }
 
     @Test
     @DisplayName("아이디가 틀렸을 때 예외처리")
     void 로그인_실패() throws Exception {
-        //given
-        LoginDto loginDto = LoginDto.builder().username("test1@gmail.com").password("123@aadfend4").build();
-        HttpEntity<LoginDto> dto = new HttpEntity<>(loginDto);
-        ResponseEntity<String> response = restTemplate.exchange(uri("/members/login"), HttpMethod.POST, dto, String.class);
-        //then
-        assertThat(response.getBody().contains(HttpStatus.UNAUTHORIZED.getReasonPhrase())).isTrue();
+        LoginDto loginDto = LoginDto.builder().username("tst@gmail.com").password("123@aadfend4").build();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String dto = objectMapper.writeValueAsString(loginDto);
+
+        mockMvc.perform(post("/members/login").contentType(MediaType.APPLICATION_JSON).content(dto))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value(401))
+                .andExpect(jsonPath("$.message").value("Unauthorized"))
+                .andDo(print());
     }
     @Test
     @DisplayName("비밀전호가 틀렸을 때 예외처리")
     void 로그인_실패2() throws Exception {
-        //given
-        LoginDto loginDto = LoginDto.builder().username("test1@gmail.com").password("23@aadfend4").build();
-        HttpEntity<LoginDto> dto = new HttpEntity<>(loginDto);
-        ResponseEntity<String> response = restTemplate.exchange(uri("/members/login"), HttpMethod.POST, dto, String.class);
-        //then
-        assertThat(response.getBody().contains(HttpStatus.UNAUTHORIZED.getReasonPhrase())).isTrue();
+        LoginDto loginDto = LoginDto.builder().username("tst@gmail.com").password("123@aadfend4").build();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String dto = objectMapper.writeValueAsString(loginDto);
+
+        mockMvc.perform(post("/members/login").contentType(MediaType.APPLICATION_JSON).content(dto))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value(401))
+                .andExpect(jsonPath("$.message").value("Unauthorized"))
+                .andDo(print());
     }
 
 
---
+++ server/src/test/java/com/sebbe013/member/service/LogoutTest.java
@@ -0,0 +1,7 @@
+package com.sebbe013.member.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LogoutTest {
+
+}
