04725ae2b4db36749618c00946e087057b2d9f77
cohi <tkfka156@gmail.com>
2022-11-05 18:41:14 +0900

    comment. 유효성 검사 정규식 메시지 추가 후 주석처리

--- server/src/main/java/com/sebbe013/member/dto/MemberSignUpDto.java
+++ server/src/main/java/com/sebbe013/member/dto/MemberSignUpDto.java
@@ -12,14 +12,16 @@ public class MemberSignUpDto {
 
     @NotBlank(message = "이메일을 입력해 주세요.")
     @Email(message = "올바른 이메일 형식이 아닙니다.")
-//    @Pattern(regexp = "^[A-Za-z0-9_]+[A-Za-z0-9]@[A-Za-z0-9]+[A-Za-z0-9][.][A-Za-z]{1,3}$")
+//    @Pattern(regexp = "^[A-Za-z0-9_]+[A-Za-z0-9]@[A-Za-z0-9]+[A-Za-z0-9][.][A-Za-z]{1,3}$",
+//             message = "@포함해서 영문 숫자로 작성")
 
     private String email;
     @NotBlank(message = "display name은 공백이 아니어야 합니다.")
     private String displayName;
 
     @NotBlank(message = "패스워드를 입력해 주세요.")
-//    @Pattern(regexp = "^(?=.[a-zA-Z])(?=.[!@#$%^+=-])(?=.[0-9]).{8,25}$")
+//    @Pattern(regexp = "^(?=.[a-zA-Z])(?=.[!@#$%^+=-])(?=.[0-9]).{8,25}$",
+//                message = "비밀번호는 숫자+영문자+특수문자 조합으로 8자리 이상 입력")
     private String password;
 
 }
