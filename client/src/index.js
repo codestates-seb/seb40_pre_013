f15fe99cee71af21337ac195a834c5a7fe85ce6a
GopOwl <viogarden16@gmail.com>
2022-11-07 17:22:05 +0900

    feat : 수정

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import axios from "axios";
 
 
-axios.defaults.baseURL = "https://1f1e-36-38-67-6.jp.ngrok.io";
+axios.defaults.baseURL = "http://ec2-54-180-125-119.ap-northeast-2.compute.amazonaws.com:8080/";
 axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
